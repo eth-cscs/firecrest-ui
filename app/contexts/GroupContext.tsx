11365b69d9bb381ab80d26ef1d464ebf5a213a13
Andrea Ceriani <andrea.ceriani@cscs.ch>
2025-12-03 09:36:02 +0100

    Group context

---
+++ app/contexts/GroupContext.tsx
@@ -0,0 +1,48 @@
+/*************************************************************************
+ Copyright (c) 2025, ETH Zurich. All rights reserved.
+
+  Please, refer to the LICENSE file in the root directory.
+  SPDX-License-Identifier: BSD-3-Clause
+*************************************************************************/
+
+import { createContext, useContext, useState } from 'react'
+
+type Group = {
+  id: string
+  name: string
+}
+
+type GroupContextValue = {
+  groups: Group[]
+  selectedGroup: Group | null
+  setSelectedGroupId: (id: string) => void
+}
+
+const GroupContext = createContext<GroupContextValue | undefined>(undefined)
+export function GroupProvider({
+  groups,
+  children,
+}: {
+  groups: Group[]
+  children: React.ReactNode
+}) {
+  const [selectedGroupId, setSelectedGroupId] = useState<string | null>(null)
+  const selectedGroup = groups.find((g) => g.id === selectedGroupId) ?? groups[0] ?? null
+  return (
+    <GroupContext.Provider
+      value={{
+        groups,
+        selectedGroup,
+        setSelectedGroupId,
+      }}
+    >
+      {children}
+    </GroupContext.Provider>
+  )
+}
+
+export function useGroup() {
+  const ctx = useContext(GroupContext)
+  if (!ctx) throw new Error('useGroup must be used within GroupProvider')
+  return ctx
+}

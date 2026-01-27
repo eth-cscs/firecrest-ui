21c28ed9024fa9860bc9c62d0c68ded0b9c2cd77
Francesco Pagnamenta <francesco.pagnamenta@cscs.ch>
2026-01-27 10:05:29 +0100

    hot fix job detail

--- app/modules/compute/components/lists/JobList.tsx
+++ app/modules/compute/components/lists/JobList.tsx
@@ -190,7 +190,7 @@ const JobTableRow: React.FC<JobTableRowProps> = ({
   )
 }
 
-const JobsTable: React.FC<any> = ({ jobs }: any) => {
+const JobsTable: React.FC<any> = ({ jobs, systemName }: any) => {
   if (jobs.length <= 0) {
     return <AlertInfo message='Job/s not found' />
   }
@@ -224,11 +224,11 @@ const JobsTable: React.FC<any> = ({ jobs }: any) => {
         <tbody>
           {jobs.map((job: Job) => (
             <JobTableRow
-              system={jobs.system}
+              system={systemName}
               key={`${job.jobId}`}
               job={job}
-              account={jobs.account}
-              user={jobs.user}
+              account={job.account}
+              user={job.user}
             />
           ))}
         </tbody>
@@ -293,7 +293,7 @@ const SystemJobList: React.FC<SystemJobListProps> = ({ jobs }) => {
         <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
         <span className='ms-3 text-sm font-medium text-gray-900 dark:text-gray-300'>All users</span>
       </label>
-      <JobsTable jobs={currentJobs ?? []} />
+      <JobsTable jobs={currentJobs ?? []} systemName={selectedSystem?.name || ''} />
     </>
   )
 }
